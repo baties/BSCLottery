6127a4d2f37e701518d3da456a2c073fe265d6a8
Batis <abba713@gmail.com>
2022-03-27 10:04:01 +0430

    Add VRF phase one

--- contracts/LotteryCore.sol
+++ contracts/LotteryCore.sol
@@ -2,12 +2,32 @@
 pragma solidity ^0.8.0;
 
 import '@openzeppelin/contracts/access/Ownable.sol';
-import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
-contract LotteryCore is Ownable, VRFConsumerBase {
+
+contract LotteryCore is VRFConsumerBaseV2 {
+
+  VRFCoordinatorV2Interface COORDINATOR;
+  LinkTokenInterface LINKTOKEN;
+
+  address vrfCoordinator = 0x6168499c0cFfCaCD319c818142124B7A15E857ab;   // Rinkeby
+  address link = 0x01BE23585060835E02B77ef475b0Cc51aA1e0709;   // Rinkeby
+  bytes32 keyHash = 0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc;   // Rinkeby
+
+  uint32 callbackGasLimit = 100000;
+  uint16 requestConfirmations = 2;
+  uint32 numWords =  1;
 
   address public lottoryOwner;
   address[] public potPlayers;
+
+  uint64 lSubscriptionId;
+  // bytes32 public reqId;
+  // uint256 public randomNumber;
+  uint256[] public lRandomWords;
+  uint256 public lRequestId;
 
   struct PotPlayer{
     uint index;
@@ -22,9 +42,14 @@ contract LotteryCore is Ownable, VRFConsumerBase {
     // mapping(address => PotPlayer) players;
   }
 
-  constructor() public {
+  // constructor(address _vrfCoordinator, address _link) VRFConsumerBase(_vrfCoordinator, _link) public {  }
+  constructor(uint64 subscriptionId) VRFConsumerBaseV2(vrfCoordinator) {
+    COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
+    LINKTOKEN = LinkTokenInterface(link);
+    lSubscriptionId = subscriptionId;
     lottoryOwner = msg.sender;
   }
+
 
   modifier ownerOnly() {
     require(msg.sender == lottoryOwner, "Owner Only! . You have not the right Access.");
@@ -33,6 +58,27 @@ contract LotteryCore is Ownable, VRFConsumerBase {
 
   fallback() external payable {
         // player name will be unknown
+  }
+
+  function requestRandomWords() external ownerOnly {
+    lRequestId = COORDINATOR.requestRandomWords(
+      keyHash,
+      lSubscriptionId,
+      requestConfirmations,
+      callbackGasLimit,
+      numWords
+    );
+  }
+
+  // function fulfillRandomness(bytes32 requestId, uint256 randomness) external override {
+  //     reqId = requestId;
+  //     randomNumber = randomness;
+  // }
+  function fulfillRandomWords(
+    uint256, /* requestId */
+    uint256[] memory randomWords
+  ) internal override {
+    lRandomWords = randomWords;
   }
 
   function balanceInPot() public view returns(uint){
