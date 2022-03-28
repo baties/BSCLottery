790fe0b13cea2a32f5f89e44216f6282f9487524
Batis <abba713@gmail.com>
2022-03-28 10:03:03 +0430

    Add & Test VRF Consumer

--- contracts/LotteryCore.sol
+++ contracts/LotteryCore.sol
@@ -5,6 +5,7 @@ import '@openzeppelin/contracts/access/Ownable.sol';
 import "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+// import "truffle/Console.sol";
 
 
 contract LotteryCore is VRFConsumerBaseV2 {
@@ -17,7 +18,7 @@ contract LotteryCore is VRFConsumerBaseV2 {
   bytes32 keyHash = 0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc;   // Rinkeby
 
   uint32 callbackGasLimit = 100000;
-  uint16 requestConfirmations = 2;
+  uint16 requestConfirmations = 3;
   uint32 numWords =  1;
 
   address public lottoryOwner;
@@ -46,19 +47,20 @@ contract LotteryCore is VRFConsumerBaseV2 {
   constructor(uint64 subscriptionId) VRFConsumerBaseV2(vrfCoordinator) {
     COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
     LINKTOKEN = LinkTokenInterface(link);
-    lSubscriptionId = subscriptionId;
     lottoryOwner = msg.sender;
+    lSubscriptionId = subscriptionId;
   }
 
+  event SelectWinner(address potWinner, uint potBalance);
 
   modifier ownerOnly() {
     require(msg.sender == lottoryOwner, "Owner Only! . You have not the right Access.");
     _;
   }
 
-  fallback() external payable {
-        // player name will be unknown
-  }
+  // fallback() external payable {
+  //       // player name will be unknown
+  // }
 
   function requestRandomWords() external ownerOnly {
     lRequestId = COORDINATOR.requestRandomWords(
@@ -90,23 +92,31 @@ contract LotteryCore is VRFConsumerBaseV2 {
     potPlayers.push(msg.sender);
   }
 
-  function randomGenerator() private view returns (uint) {
-    return uint(
-      keccak256(
-        abi.encodePacked(
-          block.difficulty, block.timestamp, potPlayers )));
-  }
+  // function randomGenerator() private view returns (uint) {
+  //   return uint(
+  //     keccak256(
+  //       abi.encodePacked(
+  //         block.difficulty, block.timestamp, potPlayers )));
+  // }
 
   function select_Send_WinnerPrize() public ownerOnly {
-    uint winnerIndex = randomGenerator() % potPlayers.length;
+    // uint winnerIndex = randomGenerator() % potPlayers.length;
+    uint winnerIndex = lRandomWords[0] % potPlayers.length;
     address payable potWinner = payable(potPlayers[winnerIndex]);
     potPlayers = new address[](0);
+    emit SelectWinner(potWinner, address(this).balance);
     potWinner.transfer(address(this).balance);
     // potPlayers[winnerIndex].transfer(address(this).balance);
+    // console.log(potWinner);
   }
 
   function listPlayers() public view returns (address[] memory){
     return potPlayers;
   }
 
+  function withdraw() external ownerOnly {
+    payable(msg.sender).transfer(address(this).balance);
+  }
+
 }
+
