80353ec333843f87d63d649536d7d1d3156d7458
Batis <abba713@gmail.com>
2022-04-02 20:08:52 +0430

    Complete Calculations and Fix a few Issues

--- contracts/LotteryCore.sol
+++ contracts/LotteryCore.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-pragma experimental ABIEncoderV2;
+// pragma experimental ABIEncoderV2;
 
 /* ToDo : Use Ownable OpenZeppelin */
 import '@openzeppelin/contracts/access/Ownable.sol';
@@ -11,17 +11,21 @@ import './VRFv2Consumer.sol' ;
 contract LotteryCore {
 
   address public lottoryOwner;
-  address[] public potPlayers;  /* Old Code may be removed soon */
+  // address[] public potPlayers;  /* Old Code may be removed soon */
 
   struct PotPlayerStr{
     uint PaymentCount ;
     uint paymentValue ;
     uint TicketNumbers ;
+    uint[] PlayersId ;
   }
 
-  PotPlayerStr[] public potPlayersArr ;
-  mapping (uint => address) public PotPlayersToAddress ;
-  mapping (address => uint) public PotPlayersToPotRate ;
+  mapping (address => PotPlayerStr) public PotPlayersMap ;
+  address[] public potPlayersArray ;
+   
+  // PotPlayerStr[] public potPlayersArr ;
+  // mapping (uint => address) public PotPlayersToAddress ;
+  // mapping (address => uint) public PotPlayersToPotRate ;
 
   /* ToDo : Generate Lottery Pot Structure as a Wrapper for Lottory Core */
   struct LotteryPot{
@@ -36,6 +40,8 @@ contract LotteryCore {
 
   event SelectWinnerIndex(uint winnerIndex, uint potBalance, uint winnerPrize) ;
   event SelectWinnerAddress(address potWinner, uint winnerPrize) ;
+  event PlayerRegister(address potPlayer, uint playerId, uint PlayerValue, uint ticketNumber) ;
+  event PlayeTotalValue(address potPlayer, uint[] playerId, uint playerValue, uint playCount, uint ticketNumber) ;
 
   constructor(address VRF) {
     lottoryOwner = msg.sender ;
@@ -58,7 +64,9 @@ contract LotteryCore {
 
   /* ToDo : Replace This function with OpenZeppelin SafeMath */
   function getDivided(uint numerator, uint denominator) public pure returns(uint quotient, uint remainder) {
-    quotient  = numerator / denominator ;
+    require( denominator >= 0, "Division is Not Possible , Bug in Numbers !") ;
+    require( numerator > 0, "Division is Not Possible , Bug in Numbers !") ;
+    quotient  = numerator / denominator ; 
     remainder = numerator - denominator * quotient ;
   }
 
@@ -66,13 +74,22 @@ contract LotteryCore {
     /* ToDo: Convert Ether to BNB */
     require(msg.value >= 0.01 ether && msg.value < 100 ether, "Value should be between 0.01 & 100 BNB") ;
     // potPlayers.push(msg.sender) ; 
-    (uint ticketNumber, ) = getDivided(msg.value, 10000000000000000) ;
-    /* ToDo: find Players old Contribution if any , 
-             And Calculate Total Value and Contribution Count */
-    potPlayersArr.push( PotPlayerStr(1, msg.value, ticketNumber) ) ;
-    uint id = potPlayersArr.length - 1 ;
-    PotPlayersToAddress[id] = msg.sender ;
-    PotPlayersToPotRate[msg.sender] = ticketNumber ;
+    (uint ticketNumber, uint remainder ) = getDivided(msg.value, 10000000000000000) ;
+    require(remainder == 0, "Value should be an Integer mutiple of 0.01 BNB") ;
+    // potPlayersArr.push( PotPlayerStr(1, msg.value, ticketNumber) ) ;
+    // uint id = potPlayersArr.length - 1 ;
+    // PotPlayersToAddress[id] = msg.sender ;
+    // PotPlayersToPotRate[msg.sender] = ticketNumber ;
+    potPlayersArray.push( msg.sender ) ;
+    uint id = potPlayersArray.length - 1 ; 
+    PotPlayersMap[msg.sender].PaymentCount += 1 ;
+    PotPlayersMap[msg.sender].paymentValue += msg.value ;
+    PotPlayersMap[msg.sender].TicketNumbers += ticketNumber ;
+    PotPlayersMap[msg.sender].PlayersId.push( id ) ;
+    emit PlayerRegister(msg.sender, id, msg.value, ticketNumber) ;
+    if (PotPlayersMap[msg.sender].PaymentCount > 1) {
+        emit PlayeTotalValue(msg.sender, PotPlayersMap[msg.sender].PlayersId, PotPlayersMap[msg.sender].PaymentCount, PotPlayersMap[msg.sender].paymentValue, PotPlayersMap[msg.sender].TicketNumbers) ;
+    }
   }
 
   // function randomGenerator() private view returns (uint) {
@@ -86,37 +103,42 @@ contract LotteryCore {
   //   VRFv2Consumer(_VRFv2).requestRandomWords(lottoryOwner) ;
   // }
 
-  function getRandomValue(address _VRFv2) public view returns (uint256 randomWords) {
+  function getRandomValue(address _VRFv2) public view ownerOnly returns (uint256 randomWords) {
     // uint8 zeroOne = uint8(randomGenerator() % 2) ;
     randomWords = VRFv2Consumer(_VRFv2).getlRandomWords() ;
   }
 
   function select_Winner() public ownerOnly {
     uint256 l_randomWords = getRandomValue(_VRF) ;
-    uint winnerIndex = l_randomWords % potPlayersArr.length ;  // potPlayers.length ;
+    uint winnerIndex = l_randomWords % potPlayersArray.length ;  // potPlayers.length ;
     uint winnerPrize = Calculation(winnerIndex) ;
     emit SelectWinnerIndex(winnerIndex, address(this).balance, winnerPrize) ;
     WinnerPrizePayment(winnerIndex, winnerPrize) ; 
     // potPlayersArr = new PotPlayerStr[](0) ;   // potPlayers
-    delete potPlayersArr ;
+    // delete potPlayersArr ;
+    delete potPlayersArray ;
   }
 
-  function Calculation(uint winnerIndex) internal returns (uint winnerPrize){
+  function Calculation(uint winnerIndex) internal view returns (uint winnerPrize){
     uint totalPot = address(this).balance ;
     /* ToDo : Complete Winner Prize Calculation */
-    winnerPrize = 
+    address WinnerAddress = potPlayersArray[winnerIndex] ;
+    uint WinnerPotAmount = PotPlayersMap[WinnerAddress].paymentValue ;
+    winnerPrize = totalPot - WinnerPotAmount ;
+    /* ToDo : Replace Calculation Parts with OpenZeppelin SafeMath ) */
+    (winnerPrize, ) = getDivided(winnerPrize, 2) ;
   }
 
   function WinnerPrizePayment(uint winnerIndex, uint winnerPrize) internal {
-    address payable potWinner = payable(PotPlayersToAddress[winnerIndex]) ;  // potPlayers 
+    address payable potWinner = payable(potPlayersArray[winnerIndex]) ;  // PotPlayersToAddress   potPlayers 
     potWinner.transfer(winnerPrize) ;
     emit SelectWinnerAddress(potWinner, winnerPrize) ;
     // potWinner.transfer(address(this).balance) ;
     //// potPlayers[winnerIndex].transfer(address(this).balance) ;
   }
 
-  function listPlayers() external view returns (PotPlayerStr[] memory){   // (address[] memory
-    return potPlayersArr ;  // potPlayers;
+  function listPlayers() external view returns (address[] memory){   // PotPlayerStr    (address[] memory   
+    return potPlayersArray ;  // potPlayersArr      potPlayers;
   }
 
   function withdraw() external ownerOnly {
