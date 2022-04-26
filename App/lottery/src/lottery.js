bbb81810599d657131213eff732d56a3efa6ac97
Batis <abba713@gmail.com>
2022-04-26 22:53:58 +0430

    Adding new FrontEnd codes .. Continue

--- App/lottery/src/lottery.js
+++ App/lottery/src/lottery.js
@@ -1,14 +1,23 @@
 import App from './App';
 import web3 from './web3';
+
+// import detectEthereumProvider from '@metamask/detect-provider';
+
 const lottery_json = require("./contracts/LotteryCore.json");
 // const lottery_json = require("./ABI/LotteryCore.js");
 
 const abi = lottery_json["abi"];
 const contractAddress = lottery_json["networks"]["4"]["address"];
 
-console.log(contractAddress);
+// console.log(contractAddress);
 
 const MyContract = new web3.eth.Contract(abi, contractAddress);  // , {from: contractAddress}
+console.log("Lottery Contract Address is : " + contractAddress);
+
+MyContract.setProvider(`wss://rinkeby.infura.io/ws/v3/${process.env.REACT_APP_INFURA_PROJECT_ID}`);
+
+// const provider = detectEthereumProvider();
+// MyContract.setProvider(provider);
 
 // export default web3.eth.contract(abi).at(address);
 export default MyContract;
